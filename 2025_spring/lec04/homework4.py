5c3a432559452912edcb6caae25719db210dc7ad
anant gaire <56.anant@gmail.com>
2025-05-26 08:56:10 +0900

    homework4 complete

--- 2025_spring/lec04/homework4.py
+++ 2025_spring/lec04/homework4.py
@@ -5,8 +5,12 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
 
+    output_dist = {}
+    for n, x in enumerate(input_list):
+        output_dist[n] = x
+    return output_dist
+    # return {index: value for index, value in enumerate(input_list)}
 
 
 
